597dfc587b3f1c8a0ab4aa5cf775fe6b1aeaf29a
Nand Vinchhi <nand@getairchat.com>
2023-09-16 05:42:31 -0400

    more ui stuff

--- frontend/src/app/open/[id]/page.tsx
+++ frontend/src/app/open/[id]/page.tsx
@@ -1,34 +1,90 @@
 'use client'
 
 import {
-    Box, HStack, Button, Text
+    Box, HStack, Button, Text, Stack
   } from '@chakra-ui/react'
   
 import { NavbarComponent  } from '@/components/Navbar/NavbarComponent'
-
-import { useEffect } from 'react'
-import { getSession } from '@/components/SupabaseFunctions'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { useEffect, useState } from 'react'
+import { getSession, supabase } from '@/components/SupabaseFunctions'
 import { useRouter } from 'next/navigation'
 
-const Home = () => {
+const Home = ({ params }: { params: { id: string } }) => {
   const router = useRouter()
+
+  const [name, setName] = useState<string>("")
   useEffect(() => {
     getSession().then(result => {
         if (!result.data.session) {
             router.push("/login")
+        } else {
+          try {
+            supabase.from('Entities').select("*").eq("id", params.id).then(res => {
+
+              if (res.data?.length > 0) {
+                setName(res.data[0].name)
+              } else {
+                router.push("/dashboard")
+              }
+            })
+          } catch {
+            router.push("/dashboard")
+          }
+         
         }
     })
   }, [])
   return (
     <Box as="section" minH="md">
       <NavbarComponent showButtons={true} isLoggedIn={true}/>
-      <Box borderBottomWidth="1px" py="4" px="4" bg="bg.surface">
+      <Box borderBottomWidth="1px" py="2" px="4" bg="bg.surface">
             <HStack spacing="2">
-                  <Text>App 1</Text>
-                      <Button colorScheme="brand" onClick={() => {router.push("/")}} variant="solid">Logout</Button>
+                  <Text>{name}</Text>
+                      
+                      <Button ml="3" size="xs" colorScheme="gray" onClick={() => {router.push("/dashboard")}} variant="solid">Dashboard</Button>
+                      <Button size="xs" colorScheme="gray" onClick={() => {router.push("/share/" + params.id)}} variant="solid">Share</Button>
                   
             </HStack>
         </Box>
+
+      <Stack spacing={0} direction="row" justify="flex-between">
+          <Box
+                borderWidth={{ base: "0", md: "1px" }}
+                p={{ base: "0", md: "4" }}
+                borderRadius="lg"
+                w="full"
+                mt="4"
+                ml="4"
+                mr="2"
+            >
+              <Text size="lg" fontWeight="bold">Inputs</Text>
+              <Button mt="3" w="full" leftIcon={<AiOutlinePlus/>}>Add</Button>
+          </Box>
+          <Box
+                borderWidth={{ base: "0", md: "1px" }}
+                p={{ base: "0", md: "4" }}
+                borderRadius="lg"
+                w="full"
+                mt="4"
+                mx="2"
+            >
+              <Text size="lg" fontWeight="bold">AI Models</Text>
+              <Button mt="3" w="full" leftIcon={<AiOutlinePlus/>}>Add</Button>
+          </Box>
+          <Box
+                borderWidth={{ base: "0", md: "1px" }}
+                p={{ base: "0", md: "4" }}
+                borderRadius="lg"
+                w="full"
+                mt="4"
+                mr="4"
+                ml="2"
+            >
+              <Text size="lg" fontWeight="bold">Outputs</Text>
+              <Button mt="3" w="full" leftIcon={<AiOutlinePlus/>}>Add</Button>
+          </Box>
+      </Stack>
     </Box>
   )
 }
